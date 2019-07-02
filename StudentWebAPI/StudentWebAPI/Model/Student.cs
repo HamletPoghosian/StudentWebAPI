b72b2f66dbb48eb5e476f98ca0c6a09eaeeb5ea2
HamletPoghosian <phamik@mail.ru>
2019-07-02 15:09:48 +0400

    add new  class

---
+++ StudentWebAPI/StudentWebAPI/Model/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentWebAPI.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Solary { get; set; }
+    }
+}
