5dc9010961154eec3556273c4b5e531dfff22df7
HamletPoghosian <phamik@mail.ru>
2019-07-02 15:08:08 +0400

    add new controller

--- StudentWebAPI/StudentWebAPI/Controllers/StudentController.cs
+++ StudentWebAPI/StudentWebAPI/Controllers/StudentController.cs
@@ -6,11 +6,65 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace StudentWebAPI.Controllers
 {
-    public class StudentController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
     {
-        public IActionResult Index()
+        List<Student> studets = new List<Student>();
+        private MyContext _myContext;
+        public StudentController(MyContext myContext)
         {
-            return View();
+            _myContext = myContext;
         }
+
+
+
+
+        [HttpGet]
+        [Route("api/Students")]
+        public ObjectResult GetJsonResult()
+        {
+            var studet = _myContext.Students;
+
+            return StatusCode(200, studet);
+        }
+
+        [HttpGet]
+        [Route("api/Students/{id}")]
+        public ObjectResult GetJsonResult(int id)
+        {
+
+
+            var studet = _myContext.Students.FirstOrDefault(s => s.Id == id);
+            if (studet != null)
+            {
+                return StatusCode(200, studet);
+            }
+            return StatusCode(404, studet);
+
+
+
+
+        }
+        [HttpPost]
+        [Route("api/AddStudentList")]
+        public ObjectResult AddStudentList(Student model)
+        {
+            try
+            {
+                _myContext.Add(model);
+                _myContext.SaveChanges();
+                return StatusCode(200, model);
+            }
+            catch (Exception ex)
+            {
+
+                return StatusCode(404, ex.Message);
+            }
+
+        }
+
+
+
     }
 }
