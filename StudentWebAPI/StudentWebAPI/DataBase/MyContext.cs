800077345a4df2dd3450ccb641907ba65125f031
HamletPoghosian <phamik@mail.ru>
2019-07-02 15:12:09 +0400

    add new db

---
+++ StudentWebAPI/StudentWebAPI/DataBase/MyContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentWebAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentWebAPI.DataBase
+{
+    public class MyContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public MyContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
